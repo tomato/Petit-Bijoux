d02cd68f30bb75e7ce76d8c21992eb0dbc004507
Tom Howlett <tom@Tom-Howletts-MacBook-Pro.local>
2010-01-13 22:33:06 +0000

    added edit link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+  $('.post').mouseover(function(){ $(this).find('.edit a').css('display','inline') }) 
+  $('.post').mouseout(function(){ $(this).find('.edit a').css('display','none') }) 
+  
+  })
